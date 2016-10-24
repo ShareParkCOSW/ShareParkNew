a6551a65c9c9ecbf0d23b0fb422a7c9b5cd928cd
DavidRR502 <david_a_6@hotmail.com>
2016-10-23 20:22:18 -0500

    Terminada configuracion ORM Vehicles

---
+++ src/main/java/edu/eci/cosw/sharepark/repositories/VehiclesRepository.java
@@ -0,0 +1,12 @@
+package edu.eci.cosw.sharepark.repositories;
+
+
+
+/**
+ * Created by David on 18/10/2016.
+ */
+
+/**
+ public interface VehiclesRepository extends JpaRepository<User, Integer>{
+ }
+ */
